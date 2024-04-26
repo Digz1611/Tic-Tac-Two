087653e83ebbbe8a74161a760dbed7d3bb5d3acf
Digz1611 <diego.langeveldt@gmail.com>
2024-04-26 10:08:39 +0200

    nts upd

--- app.js
+++ app.js
@@ -23,3 +23,22 @@ server.listen(port, () => {
 });
 
 console.log('server loaded');
+
+// const players = {}
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+//   players[socket.id] = {
+//     x: 500 * Math.random(),
+//     y: 500 * Math.random()
+//   }
+
+//   io.emit('updatePlayers', players)
+
+//   socket.on('disconnect', (reason) => {
+//   console.log(reason)
+//   delete players[socket.id]
+//   io.emit('updatePlayers', players)
+//   })
+//   console.log(players)
+// });
